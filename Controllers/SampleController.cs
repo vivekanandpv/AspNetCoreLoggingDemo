5f561588b8a689bb387ba49aabd22f218aac8fc3
Vivekanand P V <vivekanandpv@outlook.com>
2020-12-08 11:35:27 +0530

    Add project files.

---
+++ Controllers/SampleController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCoreLoggingDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        private readonly ILogger<SampleController> _logger;
+
+        public SampleController(ILogger<SampleController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            this._logger.LogInformation("Sample: Get: OK");
+            return Ok(new {Message = "Sample: OK"});
+        }
+    }
+}
