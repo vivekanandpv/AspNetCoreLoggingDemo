cd0fd8e63ae38a62a045e163fc3495c772a1571b
vivek <vivek@athenaeum.in>
2021-05-20 12:09:52 +0530

    done

--- Controllers/SampleController.cs
+++ Controllers/SampleController.cs
@@ -23,7 +23,20 @@ namespace AspNetCoreLoggingDemo.Controllers
         public IActionResult Get()
         {
             this._logger.LogInformation(ApplicationLogEvents.GetAll, "Succeeded {param1} {param2}", "Sample", "Get");
-            return Ok(new {Message = "Sample: OK"});
+            return Ok(new { Message = "Sample: OK" });
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            //  template: old api
+            this._logger.LogInformation(ApplicationLogEvents.GetById, "Succeeded for id: {param1}", id);
+
+            //  new template interpolation of C# 6
+            //  https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/interpolated
+            this._logger.LogInformation(ApplicationLogEvents.GetById, $"Succeeded for id: {id}");
+
+            return Ok(new { Message = "Sample: OK", Id = id });
         }
     }
 }
